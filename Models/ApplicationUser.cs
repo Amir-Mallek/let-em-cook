72d10acabf0bdbb4bcb2f320445aabfbf3e46cc0
Kousay Jebir <kousayjebir@gmail.com>
2025-01-25 13:31:36 +0100

    configuring blob storage

---
+++ Models/ApplicationUser.cs
@@ -0,0 +1,8 @@
+using Microsoft.AspNetCore.Identity;
+using let_em_cook.Models;
+namespace let_em_cook.Models;
+
+public class ApplicationUser : IdentityUser
+{
+
+}
--- Program.cs
+++ Program.cs
@@ -1,7 +1,9 @@
 using let_em_cook.Data;
 using Microsoft.EntityFrameworkCore;
 using DotNetEnv;
-    
+using Azure.Storage.Blobs;
+using let_em_cook.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Load environment variables from .env file
@@ -9,6 +11,11 @@ Env.Load();
 
 // Add services to the container.
 var connectionString = Environment.GetEnvironmentVariable("DefaultConnection");
+var blobStorageConnectionString = Environment.GetEnvironmentVariable("BlobStorageConnectionString");
+
+builder.Services.AddSingleton(x => new BlobServiceClient(blobStorageConnectionString));
+builder.Services.AddSingleton<BlobService>();
+
 builder.Services.AddDbContext<ApplicationdbContext>(options =>
     options.UseSqlServer(connectionString));
 
